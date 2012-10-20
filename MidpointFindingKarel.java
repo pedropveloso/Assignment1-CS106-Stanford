260bcc32e9e98cfcc1a527d39803f2254d3d97d4
S <s>
2012-10-20 23:47:18 +0100

    {"type":"TI", "runs":"1"}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -14,6 +14,70 @@ import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
 
-	// You fill in this part
+	public void run() {
+		checkOdd();
+		if (beepersPresent()) {
+			doOdd();
+		} else {
+			doEven();
+		}
+	}
+
+	private void doOdd() {
+		cleanRow();
+		
+		while (frontIsClear()) {
+			putBeeper();
+			if (frontIsClear()) {
+				move();
+				turnLeft();
+				if (frontIsClear()) {
+					move();
+					putBeeper();
+					turnRight();
+				}
+			}
+		}
+		turnLeft();
+		while (frontIsClear()) {
+			move();
+		}
+		}
+	
+	/*
+	 * a função de cleanRow serve para limpar a primeira linha novamente.
+	 * no final o Karel tem que ficar novamente na casa 1-1
+	 * virado para East, e todas as casas novamente vazias de beepers
+	 */
+	private void cleanRow() {
+		turnAround();
+		while (frontIsClear()) {
+			move();
+			if (beepersPresent()) {
+				pickBeeper();
+			}
+		}
+		turnAround();
+	}
+
+	}
+
+/*
+ * esta função apenas serve para confirmar se é par ou ímpar
+ * uma vez que as formas de actuação são ligeiramente diferentes
+ * num caso e noutro
+ * No final deste método o Karel está no canto East da rua 1, virado para East
+ */
+	private void checkOdd() {	
+		while (facingEast()) {
+			if (frontIsClear()) {
+				move();
+				if (frontIsClear()) {
+					move();
+					putBeeper();
+				}
+			}
+		}
+	}
 
 }
